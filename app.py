17f3ea16de7990d4c3755846ba1a1eacb659db86
Admin <admin@Tanveers-MacBook-Air.local>
2023-03-21 12:33:23 +0530

    adding stack practice.

---
+++ app.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self,sizeOfStack):
+        self.stack=[]
+        self.sizeOfStack=sizeOfStack
+    def push(self,val):
+        if not self.isFull():
+            self.stack.insert(0,val)
+        else:
+            print("stack is full")
+    def show(self):
+        return self.stack
+    def Pop(self):
+        if self.isEmpty():
+            print("stack is empty")
+        else:
+            self.stack.pop(0)
+    def isEmpty(self):
+        if len(self.stack)==0:
+            return True
+    def isFull(self):
+        if len(self.stack)==self.sizeOfStack:
+            return True
+    def peak(self):
+        return self.stack[0]
+ob=Stack(5)
+ob.push(6)
+print(ob.show())
+ob.push(23)
+print(ob.show())
+ob.Pop()
+print(ob.show())
+print(ob.peak())
